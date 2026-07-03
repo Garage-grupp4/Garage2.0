f08ce1b2cde877fb602f18a1cf084cd8a48c3c05
Javier Diaz Urbano <me@xavidiaz.com>
2026-07-03 15:55:00 +0200

    feat(model): svenska Display-attribut + color som string

--- Garage2.0/Models/ParkedVehicle.cs
+++ Garage2.0/Models/ParkedVehicle.cs
@@ -1,26 +1,34 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Garage2._0.Models
+namespace Garage2._0.Models;
+
+public class ParkedVehicle
 {
-    public class ParkedVehicle
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        public required string registrationNumber { get; set; }
+    [Display(Name = "Registreringsnummer")]
+    public required string registrationNumber { get; set; }
 
-        public required DateTime? arrivalTime { get; set; }
+    [Display(Name = "Ankomsttid")]
+    [DisplayFormat(DataFormatString = "{0:yy-MM-dd HH:mm}")]
+    public required DateTime? arrivalTime { get; set; }
 
-        public required VehicleType vehicleType { get; set; }
+    [Display(Name = "Fordonstyp")]
+    public required VehicleType vehicleType { get; set; }
 
-        public required string vehicleModel { get; set; }
+    [Display(Name = "Modell")]
+    public required string vehicleModel { get; set; }
 
-        public required string vehicleBrand { get; set; }
+    [Display(Name = "Märke")]
+    public required string vehicleBrand { get; set; }
 
-        public DateTime? departureTime { get; set; }
+    [Display(Name = "Utcheckningstid")]
+    public DateTime? departureTime { get; set; }
 
-        public short color { get; set; }
+    [Display(Name = "Färg")]
+    public string color { get; set; } = "";
 
-        [Range(0, 64)]
-        public required int wheels { get; set; }
-    }
+    [Display(Name = "Antal hjul")]
+    [Range(0, 64)]
+    public required int wheels { get; set; }
 }
--- Garage2.0/Models/VehicleType.cs
+++ Garage2.0/Models/VehicleType.cs
@@ -1,10 +1,19 @@
-﻿namespace Garage2._0.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage2._0.Models
 {
     public enum VehicleType
     {
+        [Display(Name = "Bil")]
         Car,
+
+        [Display(Name = "Buss")]
         Bus,
+
+        [Display(Name = "Lastbil")]
         Truck,
+
+        [Display(Name = "Motorcykel")]
         Motorcycle,
     }
 }
