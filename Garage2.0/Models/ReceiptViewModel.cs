ac4251678318ff9027a0450c3c3aa4a167bce852
George Cristian Ciolponea <georgeciolponea@outlook.com>
2026-07-06 15:07:53 +0200

    Started on issuse 5 och 6

---
+++ Garage2.0/Models/ReceiptViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Garage2._0.Models
+{
+    public class ReceiptViewModel
+    {
+        public int Id { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public VehicleType VehicleType { get; set; }
+
+        public string VehicleBrand { get; set; }
+
+        public string VehicleModel { get; set; }
+
+        public string? Color { get; set; }
+
+        public int Wheels { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public TimeSpan TotalTimeParked => DepartureTime - ArrivalTime;
+
+        public decimal TotalPrice => (decimal)TotalTimeParked.TotalHours *  50;
+    }
+}
