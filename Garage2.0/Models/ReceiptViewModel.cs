4ab6829914bd2b986556b4dc9d986f2d8014f32b
George Cristian Ciolponea <georgeciolponea@outlook.com>
2026-07-07 09:14:21 +0200

    Added check out and receipt

--- Garage2.0/Models/ReceiptViewModel.cs
+++ Garage2.0/Models/ReceiptViewModel.cs
@@ -4,13 +4,13 @@
     {
         public int Id { get; set; }
 
-        public string RegistrationNumber { get; set; }
+        public string? RegistrationNumber { get; set; }
 
         public VehicleType VehicleType { get; set; }
 
-        public string VehicleBrand { get; set; }
+        public string? VehicleBrand { get; set; }
 
-        public string VehicleModel { get; set; }
+        public string? VehicleModel { get; set; }
 
         public string? Color { get; set; }
 
@@ -22,6 +22,8 @@
 
         public TimeSpan TotalTimeParked => DepartureTime - ArrivalTime;
 
-        public decimal TotalPrice => (decimal)TotalTimeParked.TotalHours *  50;
+        private const decimal PricePerMinute = 0.5m; // Example price per minute
+
+        public decimal TotalPrice => (decimal)TotalTimeParked.TotalMinutes * PricePerMinute;
     }
 }
