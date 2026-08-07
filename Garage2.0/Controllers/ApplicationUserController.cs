f6529cf873cd659aa8c42040810e837ee464d94d
Xgor <real.xgor@hotmail.com>
2026-08-07 09:19:31 +0200

    Basic Admin page to view users

--- Garage2.0/Controllers/ApplicationUserController.cs
+++ Garage2.0/Controllers/ApplicationUserController.cs
@@ -3,14 +3,24 @@ using Garage2._0.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace Garage2._0.Controllers;
 
 public class ApplicationUserController : Controller
 {
-    [Authorize(Roles ="Admin")]
-    public IActionResult Index()
+    private UserManager<ApplicationUser> _userManager;
+
+    public ApplicationUserController(UserManager<ApplicationUser> userManager)
     {
-        return View();
+        _userManager = userManager;
+    }
+    
+    public IActionResult Index()
+    { 
+
+
+        var users = _userManager.Users.ToArray();
+        return View(users);
     }
 }
