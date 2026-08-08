85ee2b545ff78f3440e943f24a60dcc29e054ee9
Javier Diaz Urbano <me@xavidiaz.com>
2026-08-08 08:50:10 +0200

    style(US01): fix indentation in Identity files
    - Reformat to 4-space indent (matches project style)
    - Resolves Xgor's whitespace review comments

--- Garage2.0/Models/ApplicationUser.cs
+++ Garage2.0/Models/ApplicationUser.cs
@@ -5,22 +5,22 @@ namespace Garage2._0.Models;
 
 public class ApplicationUser : IdentityUser
 {
-  [Required]
-  [StringLength(50, MinimumLength = 1)]
-  [Display(Name = "Förnamn")]
-  public string FirstName { get; set; } = string.Empty;
+    [Required]
+    [StringLength(50, MinimumLength = 1)]
+    [Display(Name = "Förnamn")]
+    public string FirstName { get; set; } = string.Empty;
 
-  [Required]
-  [StringLength(50, MinimumLength = 1)]
-  [Display(Name = "Efternamn")]
-  public string LastName { get; set; } = string.Empty;
+    [Required]
+    [StringLength(50, MinimumLength = 1)]
+    [Display(Name = "Efternamn")]
+    public string LastName { get; set; } = string.Empty;
 
-  [Required]
-  [RegularExpression(
-      @"^\d{8}-\d{4}$",
-      ErrorMessage = "Personnummer måste ha formatet YYYYMMDD-XXXX"
-      )]
-  [StringLength(13, MinimumLength = 13)]
-  [Display(Name = "Personnummer")]
-  public string PersonNumber { get; set; } = string.Empty;
+    [Required]
+    [RegularExpression(
+        @"^\d{8}-\d{4}$",
+        ErrorMessage = "Personnummer måste ha formatet YYYYMMDD-XXXX"
+        )]
+    [StringLength(13, MinimumLength = 13)]
+    [Display(Name = "Personnummer")]
+    public string PersonNumber { get; set; } = string.Empty;
 }
