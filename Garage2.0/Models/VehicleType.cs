d84341177e5bd644deba59a56c7c17bf3aa086e4
Javier Diaz Urbano <me@xavidiaz.com>
2026-08-10 15:35:30 +0200

    feat(vehicle-types): lägg till validering och Display-attribut
    - [Required] + [StringLength] för robust validering
    - [Display(Name = 'Fordonstyp')] för svenska labels i vyer
    
    Refs #17 (TASK-05.3)

--- Garage2.0/Models/VehicleType.cs
+++ Garage2.0/Models/VehicleType.cs
@@ -4,10 +4,14 @@ namespace Garage2._0.Models
 {
     public class VehicleType
     {
-        public  int Id { get; set; }
+        public int Id { get; set; }
 
+        [Required(ErrorMessage = "Namn krävs.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Namn måste vara 1-50 tecken")]
+        [Display(Name = "Fordonstyp")]
         public required string Name { get; set; }
 
+        [Display(Name = "Fordon")]
         public ICollection<Vehicle> Vehicles { get; set; } = [];
     }
 }
