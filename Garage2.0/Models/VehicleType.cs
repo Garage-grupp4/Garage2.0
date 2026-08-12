6d2153500a969150dab6d33466e887347e60a8ab
Javier Diaz Urbano <me@xavidiaz.com>
2026-08-12 07:29:44 +0200

    chore(vehicle-types): översätt Display-attribut till engelska
    Åtgärdar review-kommentarer från Peter på PR (#XXX):
    - ErrorMessage nu på engelska
    - Display(Name) på engelska
    - Vehicles (plural) matchar collection-property
    
    Refs #17

--- Garage2.0/Models/VehicleType.cs
+++ Garage2.0/Models/VehicleType.cs
@@ -6,12 +6,12 @@ namespace Garage2._0.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Namn krävs.")]
-        [StringLength(50, MinimumLength = 1, ErrorMessage = "Namn måste vara 1-50 tecken")]
-        [Display(Name = "Fordonstyp")]
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Name must be 1-50 characters.")]
+        [Display(Name = "Vehicle Type")]
         public required string Name { get; set; }
 
-        [Display(Name = "Fordon")]
+        [Display(Name = "Vehicles")]
         public ICollection<Vehicle> Vehicles { get; set; } = [];
     }
 }
