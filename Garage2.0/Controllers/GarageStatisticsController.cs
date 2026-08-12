ba6d93156c8d36e9caf5580028f5030e577aa7f9
Xgor <real.xgor@hotmail.com>
2026-08-12 14:00:38 +0200

    Changed so statistics now uses real data

--- Garage2.0/Controllers/GarageStatisticsController.cs
+++ Garage2.0/Controllers/GarageStatisticsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Security.Claims;
 using Garage2._0.Constants;
+using Garage2._0.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -13,18 +14,38 @@ namespace Garage2._0.Controllers;
 [Authorize(Roles = Roles.ADMIN)]
 public class GarageStatisticsController : Controller
 {
-    public IActionResult Index()
+    private readonly GarageContext _context;
+
+    public GarageStatisticsController(
+        GarageContext context)
     {
-        // TODO use real stats
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        
+   //     var vehicles = _context.Vehicles.AsQueryable();
+  //      var vehicleCounts = vehicles.CountBy(v => v.VehicleType.Name);
+
+        var counts = await _context.Vehicles
+            .GroupBy(v => v.VehicleType.Name)
+            .Select(g => new KeyValuePair<string, int>(
+                g.Key, 
+                g.Count()
+            ))
+            .ToListAsync();
+        
         var vehicleCount = new List<KeyValuePair<string, int>>();
         vehicleCount.Add(new KeyValuePair<string, int>("Car", 2));
         vehicleCount.Add(new KeyValuePair<string, int>("Truck", 1));
         var viewModel = new GarageStatisticsViewModel()
         {
-            FreeGarageSpaces = 4,
-            OccupiedGarageSpaces = 3,
-            OutOfOrderGarageSpaces = 2,
-            VehiclesCountPerGarage = vehicleCount
+            FreeGarageSpaces = _context.ParkingSpots.Count(p => !p.IsOutOfService &&
+                p.ParkingSessions.All(ps => ps.DepartureTime != null)),
+            OccupiedGarageSpaces = _context.ParkingSpots.Count(p => p.ParkingSessions.Any(ps => ps.DepartureTime == null)),
+            OutOfOrderGarageSpaces = _context.ParkingSpots.Count(p => p.IsOutOfService),
+            VehiclesCountPerGarage = counts
             
         };
         return View(viewModel);
