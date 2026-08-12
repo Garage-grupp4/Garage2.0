fe04b920b50d491a119ca0bcd47da2f36817f2fa
Xgor <real.xgor@hotmail.com>
2026-08-12 15:13:12 +0200

    Changed statistics page to be a little bit more pretty

--- Garage2.0/Models/ViewModels/GarageStatisticsViewModel.cs
+++ Garage2.0/Models/ViewModels/GarageStatisticsViewModel.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Garage2._0.Models.ViewModels;
 
 public class GarageStatisticsViewModel
 {
     public int FreeGarageSpaces = 0;
     public int OccupiedGarageSpaces = 0;
+    
+    [Display(Name = "Out of order Garage Spaces")]
     public int OutOfOrderGarageSpaces = 0;
     public IEnumerable<KeyValuePair<string, int>> VehiclesCountPerGarage;
 }
